eb2c207ac0483372476c1c750a1474638eb5b9e0
rdpamin123 <96953025+rdpamin123@users.noreply.github.com>
2026-04-13 00:47:41 +0700

    Delete updater.js

--- updater.js
+++
@@ -1,7 +0,0 @@
-const { exec } = require("child_process")
-
-setInterval(() => {
-    exec("git pull", (err, stdout) => {
-        if (stdout) console.log("Update:", stdout)
-    })
-}, 60000) // cek tiap 1 menit
